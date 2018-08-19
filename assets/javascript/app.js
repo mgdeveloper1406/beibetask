e0445660329344162ddd5d641e097464b477d2d8
Atton <jabusau@gmail.com>
2018-08-18 21:13:02 -0700

    added functionality, need remove, maybe poop and pee, need style

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,42 +7,67 @@ $(document).ready(function() {
 
 	// Initialize Firebase
 	var config = {
-		apiKey: "AIzaSyDSWSmU55m4X-BW8FP8eTaHgUsHBsMnMx8",
-		authDomain: "scorpion-fleet-shipyard.firebaseapp.com",
-		databaseURL: "https://scorpion-fleet-shipyard.firebaseio.com",
-		projectId: "scorpion-fleet-shipyard",
-		storageBucket: "",
-		messagingSenderId: "153024076908"
+		apiKey: "AIzaSyDkLfT2P3pqFymdmdfw_zyevaM7nL4mu8U",
+		authDomain: "hollis-feeding.firebaseapp.com",
+		databaseURL: "https://hollis-feeding.firebaseio.com",
+		projectId: "hollis-feeding",
+		storageBucket: "hollis-feeding.appspot.com",
+		messagingSenderId: "184476627198"
 	};
 	firebase.initializeApp(config);
 
 	var database = firebase.database();
 
+	// display placeholder times
+	$("#timeText").text(moment().format("HHmm"));
+	$("#dateInput").attr("placeholder", new Date().toLocaleDateString());
+	$("#timeInput").attr("placeholder", moment().format("HHmm"));
+
+
 	// On-click of submit button function, takes values and sends to FireBase
 	$("#submitButton").on("click", function(event) {
 		event.preventDefault();
-	  
-		// Grabs user input from form
-		var name = $("#nameInput").val().trim();
-		var destination = $("#destinationInput").val().trim();
-		var startTime = $("#timeInput").val().trim();
-		var frequency = $("#frequencyInput").val().trim();
-	  
-		// Creates local "temporary" object for holding employee data
-		var newRoute = {
-			name: name,
-			dest: destination,
-			time: startTime,
-			freq: frequency
-		};
+		
+		if ($("#amountInput").val() || $("#lengthInput").val()) {
+			var timeAuto = moment().subtract($("#lengthInput").val().trim(), "m").format("HHmm");
 
-		// console.log(newRoute); //test
+			// Grabs user input from form
+			if ($("#dateInput").val() == "") {
+				var date = new Date().toLocaleDateString();
+			} else {
+				var date = $("#dateInput").val().trim();
+			}
+			if ($("#timeInput").val() == "") {
+				var time = timeAuto;
+			} else {
+				var time = $("#timeInput").val().trim();
+			}
+			var type = $("#typeInput").val().trim();
+			var amount = $("#amountInput").val().trim();
+			var length = $("#lengthInput").val().trim();
 
-		// Send new route to FireBase
-		database.ref().push(newRoute);
+			// Creates local "temporary" object for holding employee data
+			var newRoute = {
+				date: date,
+				time: time,
+				type: type,
+				amount: amount,
+				length: length
+			};
 
-		// Resets forms
-		$("#addForm")[0].reset();
+			// Send new route to FireBase
+			database.ref().push(newRoute);
+
+			// Resets forms
+			$("#dateInput").val("");
+			$("#timeInput").val("");
+			$("#typeInput").val("");
+			$("#amountInput").val("");
+			$("#lengthInput").val("");
+		}
+		else {
+			alert("Enter amount and length");
+		}
 	})
 
 
@@ -51,38 +76,43 @@ $(document).ready(function() {
 	database.ref().on("child_added", function(childSnapshot) {
 
 		// Get current time and append
-		$("#timeText").text(moment().format("hh:mm a"));
 
 		// console.log(childSnapshot.val()); //test
 		
 		// Save FireBase routes to variables
-		var name = childSnapshot.val().name;
-		var destination = childSnapshot.val().dest;
-		var startTime = childSnapshot.val().time;
-		var frequency = childSnapshot.val().freq;
+		var date = childSnapshot.val().date;
+		var time = childSnapshot.val().time;
+		var type = childSnapshot.val().type;
+		var amount = childSnapshot.val().amount;
+		var length = childSnapshot.val().length;
 
-		console.log(name, destination, startTime, frequency); //test
 
-		// Add frequency to time until it is equal or greater than current time
-		var timeNow = moment();
-		startTime = moment(startTime, "HHmm");
+		console.log(date, time, type, amount, length); //test
 
-		// if start time is negative, add frequency to start time until non-negative
-		if (timeNow.diff(startTime, "m") > 0) {
-			while (timeNow.diff(startTime, "m") > 0) {
-				startTime.add(frequency, "m");
-			}
-		}
+		// // Add frequency to time until it is equal or greater than current time
+		// var timeNow = moment();
+		// startTime = moment(startTime, "HHmm");
+
+		// // if start time is negative, add frequency to start time until non-negative
+		// if (timeNow.diff(startTime, "m") > 0) {
+		// 	while (timeNow.diff(startTime, "m") > 0) {
+		// 		startTime.add(frequency, "m");
+		// 	}
+		// }
 
 		// Append routes to table
 		var newRoute = $("<tr>").append(
-			$("<td>").text(name),
-			$("<td>").text(destination),
-			$("<td>").text(frequency),
-			$("<td>").text(startTime.format("hh:mm a")),
-			// Add 1 and Math.floor to solve rounding problems
-			$("<td>").text(Math.floor(startTime.diff(timeNow, "m") + 1))
+			$("<td>").text(date),
+			$("<td>").text(time),
+			$("<td>").text(type),
+			$("<td>").text(amount),
+			$("<td>").text(length)
 		)
-		$("#routeText").append(newRoute); // Append new div
+		$("#feedText").append(newRoute); // Append new div
 	})
-})
+
+		$("#lengthInput").change(function() {
+			$("#timeInput").attr("placeholder", moment().subtract($("#lengthInput").val().trim(), "m").format("HHmm"));
+			$("#lengthInput").val()
+		});
+	})
